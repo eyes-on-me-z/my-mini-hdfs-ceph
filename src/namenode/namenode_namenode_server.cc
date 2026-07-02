fe8be4ecfd97b3b6b581b7659ad9cdf2161eca7f
loser <1757770113@qq.com>
2026-07-02 21:39:03 +0800

    namenode server

---
+++ src/namenode/namenode_namenode_server.cc
@@ -0,0 +1,8 @@
+#include "namenode_namenode_server.h"
+
+namespace mini_storage
+{
+
+    
+    
+} // namespace mini_storage
---
+++ src/namenode/namenode_namenode_server.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "namenode_metadata_store.h"
+#include "namenode_block_manager.h"
+#include "namenode_datanode_manager.h"
+#include "namenode_edit_log.h"
+#include "namenode_fault_detector.h"
+#include "net_event_loop.h"
+#include "net_tcp_server.h"
+#include "net_thread_pool.h"
+
+#include <string>
+#include <memory>
+
+namespace mini_storage
+{
+    class TcpConnection;
+
+    class NameNodeServer
+    {
+
+        
+    private:
+        void OnMessage(std::shared_ptr<TcpConnection> conn, const std::string &data);
+        void ProcessRequest(std::shared_ptr<TcpConnection> conn, const std::string &data);
+
+        NameNodeResponse HandleCreateFile(const CreateFileRequest &req);
+        NameNodeResponse HandleAllocateBlock(const AllocateBlockRequest &req);
+        NameNodeResponse HandleGetFileBlocks(const GetFileBlocksResponse &req);
+
+        std::string data_dir_;
+        std::string host_;
+        int port_;
+
+        std::unique_ptr<MetadataStore> metadata_;
+        std::unique_ptr<BlockManager> block_manager_;
+        std::unique_ptr<DataNodeManager> dn_manager_;
+        std::unique_ptr<EditLog> edit_log_;
+        std::unique_ptr<FaultDetector> fault_detector_;
+
+        std::unique_ptr<EventLoop> loop_;
+        std::unique_ptr<TcpServer> server_;
+        std::unique_ptr<ThreadPool> thread_pool_;
+
+        std::thread health_check_thread_;
+        std::atomic<bool> stop_{false};
+    };
+} // namespace mini_storage
